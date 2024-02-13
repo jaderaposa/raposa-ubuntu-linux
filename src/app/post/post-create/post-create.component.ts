adb9d4ab75b141ef0efeea1701e1998b61377206
jaderaposa <jaduya678@gmail.com>
2024-02-13 18:47:45 +0800

    Refactor post-create component HTML and CSS

--- src/app/post/post-create/post-create.component.ts
+++ src/app/post/post-create/post-create.component.ts
@@ -3,23 +3,23 @@ import { Post } from "../post.model";
 import { NgForm } from "@angular/forms";
 
 @Component({
-    selector: "app-post-create",
-    templateUrl: "./post-create.component.html",
-    styleUrls: ["./post-create.component.css"],
+	selector: "app-post-create",
+	templateUrl: "./post-create.component.html",
+	styleUrls: ["./post-create.component.css"],
 })
 export class PostCreateComponent {
-    enteredTitle = "";
-    enteredContent = "";
+	enteredTitle = "";
+	enteredContent = "";
 
-    @Output() postCreated = new EventEmitter<Post>();
-    onAddPost(form: NgForm) {
-        if (form.invalid) {
-            return;
-        }
-        const post: Post = {
-            title: form.value.title,
-            content: form.value.content,
-        };
-        this.postCreated.emit(post);
-    }
+	@Output() postCreated = new EventEmitter<Post>();
+	onAddPost(form: NgForm) {
+		if (form.invalid) {
+			return;
+		}
+		const post: Post = {
+			title: form.value.title,
+			content: form.value.content,
+		};
+		this.postCreated.emit(post);
+	}
 }
