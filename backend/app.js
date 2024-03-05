4c2ccb287e1b281749ffebc93274e524681c265a
jaderaposa <jaduya678@gmail.com>
2024-03-05 18:43:20 +0800

    Add id field to Post interface and update PostService to handle id field in addPost method

--- backend/app.js
+++ backend/app.js
@@ -1,47 +1,71 @@
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require("express");
+const bodyParser = require("body-parser");
+const Post = require("./models/posts"); // Import the Post model
+const mongoose = require("mongoose");
 const cors = require("cors");
 const app = express();
+
+mongoose
+    .connect("mongodb+srv://jaderaposa:jademongodatabase@jadedb.gb4oedl.mongodb.net/node-angular?retryWrites=true&w=majority&appName=JadeDB")
+    .then(() => {
+        console.log("Connected to database!");
+    })
+    .catch(() => {
+        console.log("Connection failed!");
+    });
 
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use((req, res, next) => {
-	res.setHeader("Access-Control-Allow-Origin", "*");
-	res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-	res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE, OPTIONS");
-	next();
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE, OPTIONS");
+    next();
 });
 
-
 app.post("/api/posts", (req, res, next) => {
-	const posts = req.body;
-	console.log(posts);
-	res.status(201).json({ message: "Post added!" });
+  const post = new Post({
+    title: req.body.title,
+    content: req.body.content
+  });
+  post.save().then(createdPost => {
+    res.status(201).json({
+      message: "Post added successfully",
+      postId: createdPost._id
+    });
+  });
 });
 
 app.get("/api/posts", (req, res, next) => {
-	const posts = [
-		{
-			id: "fadf12421l",
-			title: "First server-side post",
-			content: "This is coming from the server",
-		},
-		{
-			id: "ksajflaj132",
-			title: "Second server-side post",
-			content: "This is coming from the server!",
-		},
-	];
-	res.status(200).json({
-		message: "Posts fetched successfully!",
-		posts: posts,
-	});
+  const posts = [
+    {
+      id: "fadf12421l",
+      title: "First server-side post",
+      content: "This is coming from the server",
+    },
+    {
+      id: "ksajflaj132",
+      title: "Second server-side post",
+      content: "This is coming from the server!",
+    },
+  ];
+
+  Post.find().then(documents => {
+    const allPosts = posts.concat(documents);
+    res.status(200).json({
+      message: "Posts fetched successfully!",
+      posts: allPosts,
+    });
+  });
 });
 
 app.get("/posts", (req, res, next) => {
-	res.json(posts);
+  Post.find().then(documents => {
+    const allPosts = posts.concat(documents);
+    res.json(allPosts);
+  });
 });
 
 module.exports = app;
